8612ac74965acb53a03d4f66a3975d38e5f326d3
Khush Isway <149180474+thekhush11@users.noreply.github.com>
2025-07-17 15:23:56 +0530

    Delete Artificial Intelligence directory

--- Artificial Intelligence/src/config/settings.py
+++
@@ -1,7 +0,0 @@
-API_KEY = "your_api_key_here"
-DEFAULT_VOLUME = 1.0
-MUSIC_DIRECTORY = "path_to_your_music_directory"
-SMS_SERVICE_SID = "your_twilio_service_sid"
-SMS_AUTH_TOKEN = "your_twilio_auth_token"
-SMS_FROM_NUMBER = "your_twilio_from_number"
-WIKIPEDIA_LANGUAGE = "en"
